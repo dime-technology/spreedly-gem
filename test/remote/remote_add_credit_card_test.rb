5ffe7473ae7e621aa037dccf27fb842d2de5b179
Alessandro Stein <alessandro@alessandrostein.com>
2019-12-03 19:16:19 -0300

    Add the ability to add a bank account

--- test/remote/remote_add_credit_card_test.rb
+++ test/remote/remote_add_credit_card_test.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class RemoteAddCreditCardTest < Test::Unit::TestCase
-
   def setup
     @environment = Spreedly::Environment.new(remote_test_environment_key, remote_test_access_secret)
   end
@@ -18,8 +19,8 @@ class RemoteAddCreditCardTest < Test::Unit::TestCase
     end
 
     expected_errors = [
-      { attribute: "first_name", key: "errors.blank", message: "First name can't be blank" },
-      { attribute: "last_name", key: "errors.blank", message: "Last name can't be blank" }
+      { attribute: 'first_name', key: 'errors.blank', message: "First name can't be blank" },
+      { attribute: 'last_name', key: 'errors.blank', message: "Last name can't be blank" }
     ]
 
     assert_equal expected_errors, error.errors
@@ -27,7 +28,7 @@ class RemoteAddCreditCardTest < Test::Unit::TestCase
   end
 
   def test_payment_required
-    assert_raise_with_message(Spreedly::PaymentRequiredError, "Your environment (R7lHscqcYkZeDGGbthKp6GKMu15) has not been activated for real transactions with real payment methods. If you're using a Test Gateway you can *ONLY* use Test payment methods - ( https://docs.spreedly.com/test-data). All other credit card numbers are considered real credit cards; real credit cards are not allowed when using a Test Gateway.") do
+    assert_raise_with_message(Spreedly::PaymentRequiredError, "Your environment (#{remote_test_environment_key}) has not been activated for real transactions with real payment methods. If you're using a Test Gateway you can *ONLY* use Test payment methods - ( https://docs.spreedly.com/test-data). All other credit card numbers are considered real credit cards; real credit cards are not allowed when using a Test Gateway.") do
       @environment.add_credit_card(card_deets(number: '343'))
     end
   end
@@ -51,20 +52,19 @@ class RemoteAddCreditCardTest < Test::Unit::TestCase
   end
 
   def test_successfull_add_using_full_name
-    t = @environment.add_credit_card(number: '5555555555554444', month: 1, year: 2023, full_name: "Kvothe Jones")
+    t = @environment.add_credit_card(number: '5555555555554444', month: 1, year: 2023, full_name: 'Kvothe Jones')
     assert t.succeeded?
-    assert_equal "Kvothe", t.payment_method.first_name
-    assert_equal "Jones", t.payment_method.last_name
-    assert_equal "Kvothe Jones", t.payment_method.full_name
+    assert_equal 'Kvothe', t.payment_method.first_name
+    assert_equal 'Jones', t.payment_method.last_name
+    assert_equal 'Kvothe Jones', t.payment_method.full_name
   end
 
-
   private
+
   def card_deets(options = {})
     {
       email: 'perrin@wot.com', number: '5555555555554444', month: 1, year: 2023,
-      last_name: 'Aybara', first_name: 'Perrin', data: "occupation: Blacksmith"
+      last_name: 'Aybara', first_name: 'Perrin', data: 'occupation: Blacksmith'
     }.merge(options)
   end
-
 end
