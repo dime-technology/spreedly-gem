47efebf04c22b4d3287b8986c1db75e9cb50294a
Alessandro Stein <alessandro@alessandrostein.com>
2019-12-04 13:27:30 -0300

    Update payment_method.rb

--- lib/spreedly/payment_methods/payment_method.rb
+++ lib/spreedly/payment_methods/payment_method.rb
@@ -1,9 +1,9 @@
+# frozen_string_literal: true
+
 require 'time'
 
 module Spreedly
-
   class PaymentMethod < Model
-
     include ErrorsParser
 
     field :email, :storage_state, :data, :payment_method_type
@@ -17,29 +17,27 @@ module Spreedly
     def self.new_from(xml_doc)
       case xml_doc.at_xpath('.//payment_method_type').inner_text
       when 'credit_card'
-        return CreditCard.new(xml_doc)
+        CreditCard.new(xml_doc)
       when 'paypal'
-        return Paypal.new(xml_doc)
+        Paypal.new(xml_doc)
       when 'sprel'
-        return Sprel.new(xml_doc)
+        Sprel.new(xml_doc)
       when 'bank_account'
-        return BankAccount.new(xml_doc)
+        BankAccount.new(xml_doc)
       when 'third_party_token'
-        return ThirdPartyToken.new(xml_doc)
+        ThirdPartyToken.new(xml_doc)
       end
     end
 
     def self.new_list_from(xml_doc)
       payment_methods = xml_doc.xpath('.//payment_methods/payment_method')
       payment_methods.map do |each|
-        self.new_from(each)
+        new_from(each)
       end
     end
 
     def valid?
       @errors.empty?
     end
-
   end
-
 end
